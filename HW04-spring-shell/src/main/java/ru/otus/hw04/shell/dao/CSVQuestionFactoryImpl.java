a0868874b1940eefdab7263a003fe3addf7c2f98
s1t3w4n <s1t3w4n@yandex.ru>
2020-04-19 15:49:59 +0300

    HW04 changed QuestionFactory List to Map

--- HW04-spring-shell/src/main/java/ru/otus/hw04/shell/dao/CSVQuestionFactoryImpl.java
+++ HW04-spring-shell/src/main/java/ru/otus/hw04/shell/dao/CSVQuestionFactoryImpl.java
@@ -7,32 +7,38 @@ import ru.otus.hw04.shell.app.CSVQuestionFactory;
 import ru.otus.hw04.shell.dao.creators.QuestionCreator;
 import ru.otus.hw04.shell.data.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static ru.otus.hw04.shell.dao.CSVHeaders.*;
 
 @Service
 class CSVQuestionFactoryImpl implements CSVQuestionFactory {
 
-    private final List<QuestionCreator> creators;
+    private final Map<QuestionType, QuestionCreator> creators;
 
     @Autowired
     public CSVQuestionFactoryImpl(List<QuestionCreator> creators) {
-        this.creators = creators;
+        this.creators = fillCreators(creators);
     }
 
     @Override
     public Question getQuestion(CSVRecord record) {
 
         QuestionType questionType = QuestionType.valueOf(record.get(type).toUpperCase());
-        for (QuestionCreator creator : creators) {
-            if (creator.getType().equals(questionType)) {
-                return creator.createQuestion(record.get(body),
-                        record.get(correct),
-                        record.get(wrong));
-            }
-        }
-        return null;
+        if (creators.containsKey(questionType)) {
+            return creators.get(questionType).createQuestion(record.get(body),
+                    record.get(correct),
+                    record.get(wrong));
+        } else return null;
     }
 
+    private Map<QuestionType, QuestionCreator> fillCreators(List<QuestionCreator> creators) {
+        HashMap<QuestionType, QuestionCreator> questionTypeQuestionCreatorHashMap = new HashMap<>();
+        for (QuestionCreator creator : creators) {
+            questionTypeQuestionCreatorHashMap.put(creator.getType(), creator);
+        }
+        return questionTypeQuestionCreatorHashMap;
+    }
 }
