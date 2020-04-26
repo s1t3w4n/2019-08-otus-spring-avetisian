2ae94fa374e3b8eb0cf59ddad676b5e23430b322
s1t3w4n <s1t3w4n@yandex.ru>
2020-04-26 14:22:17 +0300

    HW05 starting

---
+++ HW05-sping-jdbc/src/test/java/ru/otus/hw05/Hw05SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw05SpringJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
