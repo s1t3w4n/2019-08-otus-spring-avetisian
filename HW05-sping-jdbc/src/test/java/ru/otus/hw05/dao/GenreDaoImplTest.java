c17047595850c21126a8b46f5b06365dfde8e508
s1t3w4n <s1t3w4n@yandex.ru>
2020-05-01 15:34:30 +0300

    HW05 have added test on genre dao

--- HW05-sping-jdbc/src/test/java/ru/otus/hw05/dao/GenreDaoImplTest.java
+++ HW05-sping-jdbc/src/test/java/ru/otus/hw05/dao/GenreDaoImplTest.java
@@ -1,20 +1,47 @@
 package ru.otus.hw05.dao;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw05.domain.Genre;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Dao for working with Genre have to:")
+@JdbcTest
+@Import(GenreDaoImpl.class)
 class GenreDaoImplTest {
 
+    private static final long EXPECTED_INSERTED_ID = 2L;
+    private static final long DEFAULT_GENRE_ID = 1L;
+    private static final String DEFAULT_GENRE = "novel";
+    @Autowired
+    private GenreDao genreDao;
+
+    @DisplayName("Should insert Genre in Data Base")
     @Test
-    void insert() {
+    void shouldInsertGenreInDataBase() {
+        Genre expected = new Genre(EXPECTED_INSERTED_ID, "criminal");
+        genreDao.insert(expected);
+        Genre actual = genreDao.getById(EXPECTED_INSERTED_ID);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 
+    @DisplayName("Should return Genre with current id")
     @Test
-    void getById() {
+    void shouldReturnGenreById() {
+        Genre expected = new Genre(DEFAULT_GENRE_ID, DEFAULT_GENRE);
+        Genre actual = genreDao.getById(DEFAULT_GENRE_ID);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 
+    @DisplayName("Should return Genre with current genre")
     @Test
-    void getByGenre() {
+    void shouldReturnGenreByGenre() {
+        Genre expected = new Genre(DEFAULT_GENRE_ID, DEFAULT_GENRE);
+        Genre actual = genreDao.getByGenre(DEFAULT_GENRE);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 }
