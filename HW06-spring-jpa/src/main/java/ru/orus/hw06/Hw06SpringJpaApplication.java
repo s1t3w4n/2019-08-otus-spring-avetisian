daabcfe28a7cd72d908ba1dde2e2fe3db9b04fc2
s1t3w4n <s1t3w4n@yandex.ru>
2020-05-10 13:46:24 +0300

    HW06 1st commit

---
+++ HW06-spring-jpa/src/main/java/ru/orus/hw06/Hw06SpringJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.orus.hw06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw06SpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw06SpringJpaApplication.class, args);
+    }
+
+}
