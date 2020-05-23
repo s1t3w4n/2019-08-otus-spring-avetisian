41a8088a030ead9026e5120f99a413364faea76b
s1t3w4n <s1t3w4n@yandex.ru>
2020-05-23 21:35:55 +0300

    HW07 1st commit

---
+++ HW07-spring-data/src/main/java/ru/otus/hw07/Hw07SpringDataApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw07SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw07SpringDataApplication.class, args);
+    }
+
+}
---
+++ HW07-spring-data/src/test/java/ru/otus/hw07/Hw07SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw07SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
