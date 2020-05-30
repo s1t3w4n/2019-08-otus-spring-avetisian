4ea24a288d0e87e6513082fb51799211c6ee0535
s1t3w4n <s1t3w4n@yandex.ru>
2020-05-30 21:43:20 +0300

    HW08 1st commit

---
+++ HW08-spring-mongo-db/src/main/java/ru/otus/hw08/Hw08SpringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw08SpringMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw08SpringMongoDbApplication.class, args);
+    }
+
+}
---
+++ HW08-spring-mongo-db/src/test/java/ru/otus/hw08/Hw08SpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw08SpringMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
