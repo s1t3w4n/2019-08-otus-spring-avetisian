6c3bc85114ce173c86efb6bfc98977c8dc8c1dcc
s1t3w4n <s1t3w4n@yandex.ru>
2020-06-11 12:42:25 +0300

    HW08 Spring Boot 2.2.6 valid mongock cofiguration

--- HW08-spring-mongo-db/src/main/java/ru/otus/hw08/mongock/ApplicationConfig.java
+++ HW08-spring-mongo-db/src/main/java/ru/otus/hw08/mongock/ApplicationConfig.java
@@ -1,10 +1,11 @@
 package ru.otus.hw08.mongock;
 
-import com.github.cloudyrock.mongock.Mongock;
+import com.github.cloudyrock.mongock.SpringMongock;
 import com.github.cloudyrock.mongock.SpringMongockBuilder;
-import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class ApplicationConfig {
@@ -12,8 +13,10 @@ public class ApplicationConfig {
     private static final String CHANGELOGS_PACKAGE = "ru.otus.hw08.mongock.changelog";
 
     @Bean
-    public Mongock mongock(MongoProps mongoProps, MongoClient mongoClient) {
-        return new SpringMongockBuilder(mongoClient, mongoProps.getDatabase(), CHANGELOGS_PACKAGE)
+    public SpringMongock springMongock(MongoTemplate mongoTemplate, Environment springEnvironment) {
+        return new SpringMongockBuilder(mongoTemplate, CHANGELOGS_PACKAGE)
+                .setSpringEnvironment(springEnvironment)
+                .setLockQuickConfig()
                 .build();
     }
 }
