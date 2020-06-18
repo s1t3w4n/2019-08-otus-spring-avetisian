0a92f9f9044a439e383a1ed4610b7a4cddad75de
s1t3w4n <s1t3w4n@yandex.ru>
2020-06-18 12:08:36 +0300

    HW09 1st commit

---
+++ HW09-spring-mvc-classic/src/main/java/ru/otus/hw09/Hw09SpringMvcClassicApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw09SpringMvcClassicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw09SpringMvcClassicApplication.class, args);
+    }
+
+}
---
+++ HW09-spring-mvc-classic/src/test/java/ru/otus/hw09/Hw09SpringMvcClassicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw09SpringMvcClassicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
