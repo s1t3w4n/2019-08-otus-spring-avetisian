6c0434026c6a8ba05f1f25f3a3e029bc6dfcc457
s1t3w4n <s1t3w4n@yandex.ru>
2020-07-10 20:34:44 +0300

    HW10 has changed rest path for book's comments

--- HW10-spring-mvc-jquery/src/main/java/ru/otus/hw10/rest/CommentRestController.java
+++ HW10-spring-mvc-jquery/src/main/java/ru/otus/hw10/rest/CommentRestController.java
@@ -1,7 +1,7 @@
 package ru.otus.hw10.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw10.rest.dto.CommentDto;
 import ru.otus.hw10.service.LibraryService;
@@ -23,8 +23,8 @@ public class CommentRestController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("api/comments/book")
-    public List<CommentDto> getBookComments(@RequestParam long id) {
+    @GetMapping("api/comments/book/{id}")
+    public List<CommentDto> getBookComments(@PathVariable long id) {
         return service.getBookComments(id).stream().map(CommentDto::toDto)
                 .collect(Collectors.toList());
     }
