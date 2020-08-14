ab31164ced592777cc446e40a8f5b7e9b48140e4
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-14 22:03:36 +0300

    HW12 1st commit

---
+++ HW12-spring-security-autentication/src/main/java/ru/orus/hw12/Hw12SpringSecurityAutenticationApplication.java
@@ -0,0 +1,13 @@
+package ru.orus.hw12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw12SpringSecurityAutenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw12SpringSecurityAutenticationApplication.class, args);
+    }
+
+}
---
+++ HW12-spring-security-autentication/src/test/java/ru/orus/hw12/Hw12SpringSecurityAutenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.orus.hw12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw12SpringSecurityAutenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
