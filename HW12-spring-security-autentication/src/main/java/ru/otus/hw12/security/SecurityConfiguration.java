ebbacf43fee3e62656cc4e8804c9f90025c4bf65
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-15 20:12:17 +0300

    HW12 has added simple SecurityConfiguration

---
+++ HW12-spring-security-autentication/src/main/java/ru/otus/hw12/security/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package ru.otus.hw12.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .formLogin();
+    }
+}
