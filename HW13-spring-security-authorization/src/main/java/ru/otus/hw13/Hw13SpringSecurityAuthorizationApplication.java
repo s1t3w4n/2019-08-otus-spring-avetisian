96c872a56d4440dc42200b9c91f86dc022f8f518
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-19 10:35:10 +0300

    HW13 1st commit

---
+++ HW13-spring-security-authorization/src/main/java/ru/otus/hw13/Hw13SpringSecurityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw13SpringSecurityAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw13SpringSecurityAuthorizationApplication.class, args);
+    }
+
+}
---
+++ HW13-spring-security-authorization/src/test/java/ru/otus/hw13/Hw13SpringSecurityAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw13SpringSecurityAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
