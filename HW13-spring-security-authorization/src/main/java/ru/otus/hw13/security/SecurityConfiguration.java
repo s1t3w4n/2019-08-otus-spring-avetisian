a25a20a3656cb0c9a1d9196a38cf7eb1c4f76f25
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-20 18:53:45 +0300

    HW13 has added acl config

--- HW13-spring-security-authorization/src/main/java/ru/otus/hw13/security/SecurityConfiguration.java
+++ HW13-spring-security-authorization/src/main/java/ru/otus/hw13/security/SecurityConfiguration.java
@@ -1,14 +1,12 @@
 package ru.otus.hw13.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
--- HW13-spring-security-authorization/src/main/resources/data.sql
+++ HW13-spring-security-authorization/src/main/resources/data.sql
@@ -16,3 +16,26 @@ insert into users (id, login, password, role) values
 (1, 'admin', '$2y$06$2toScVggylkdjSm7jsfwhec/4afHqHh6ElmIkSKpzxxri9ZbrtzSe', 'ADMIN'),
 (2, 'user', '$2y$06$wr/Ml.SjcoQNAv/UYtciz.zBdSHw3FTc/3WQe5JIMx4yfm4oC97XK', 'USER'),
 (3, 'guest', '$2y$06$ilavE6lBH8zJhon89gMY6OFZTVL/naEP1a4eGpJ4JRiClStyrMa56', 'GUEST');
+
+-- acl
+
+INSERT INTO acl_sid (id, principal, sid) VALUES
+(1, 1, 'admin');
+
+INSERT INTO acl_class (id, class) VALUES
+(1, 'ru.otus.hw13.models.Comment');
+
+-- INSERT INTO system_message(id,content) VALUES
+-- (1,'First Level Message'),
+-- (2,'Second Level Message'),
+-- (3,'Third Level Message');
+
+INSERT INTO acl_object_identity (id, object_id_class, object_id_identity, parent_object, owner_sid, entries_inheriting) VALUES
+(1, 1, 1, NULL, 1, 0),
+(2, 1, 2, NULL, 1, 0),
+(3, 1, 3, NULL, 1, 0);
+
+INSERT INTO acl_entry (id, acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure) VALUES
+(1, 1, 1, 1, 0, 1, 1, 1),
+(2, 2, 2, 1, 0, 1, 1, 1),
+(3, 3, 3, 1, 0, 1, 1, 1);
