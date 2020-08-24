b4ce119755928ce636f0b98a23e825f4143ceb1a
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-24 13:34:31 +0300

    HW14 1st commit

---
+++ HW14-spring-batch/src/main/java/ru/otus/hw14/Hw14SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw14SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw14SpringBatchApplication.class, args);
+    }
+
+}
