68dc4592877355b6ff2df14e39963d1b96953d80
s1t3w4n <s1t3w4n@yandex.ru>
2020-08-30 14:22:18 +0300

    HW15 1st commit

---
+++ HW15-spring-integraion/src/test/java/ru/otus/hw15/Hw15SpringIntegraionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.hw15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw15SpringIntegraionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
